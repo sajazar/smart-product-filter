ca7529c37c93a23940352fcd58210c101cca6fe0
sajazar <143828773+sajazar@users.noreply.github.com>
2026-09-24 13:11:16 +0330

    Use WooCommerce archive GET filtering with history support

--- smart-product-filter/assets/js/script.js
+++ smart-product-filter/assets/js/script.js
@@ -1,8 +1,13 @@
 (function($) {
     'use strict';
 
-    function initFilter($wrap) {
+    var request = null;
 
+    function formatPrice(n) {
+        return Number(n).toLocaleString('fa-IR');
+    }
+
+    function initFilter($wrap) {
         var $minRange = $wrap.find('.spf-range-min');
         var $maxRange = $wrap.find('.spf-range-max');
         var $range    = $wrap.find('.spf-slider-range');
@@ -13,11 +18,11 @@
         var absMax = parseFloat($wrap.data('max')) || 10000000;
         var step   = parseFloat($wrap.data('step')) || 1;
 
-        function formatPrice(n) {
-            return Number(n).toLocaleString('fa-IR');
-        }
-
         function updateSlider() {
+            if (!$minRange.length || !$maxRange.length) {
+                return;
+            }
+
             var minVal = parseFloat($minRange.val()) || absMin;
             var maxVal = parseFloat($maxRange.val()) || absMax;
 
@@ -52,58 +57,32 @@
 
         function getPriceValues() {
             return {
-                min: parseFloat($minRange.val()) || absMin,
-                max: parseFloat($maxRange.val()) || absMax
+                min: $minRange.length ? (parseFloat($minRange.val()) || absMin) : absMin,
+                max: $maxRange.length ? (parseFloat($maxRange.val()) || absMax) : absMax
             };
         }
 
         function getTermUrlBySlug(slug) {
-            var $checkbox = $wrap.find('.spf-cat-checkbox').filter(function() {
+            return $wrap.find('.spf-cat-checkbox').filter(function() {
                 return this.value === slug;
-            }).first();
-
-            return $checkbox.attr('data-term-url') || '';
+            }).first().attr('data-term-url') || '';
         }
 
-        /**
-         * URL مرورگر:
-         * - یک دسته: canonical term URL ووکامرس، بنابراین hierarchy والد/فرزند حفظ می‌شود.
-         * - چند دسته: shop/?product_cat=cat-a,cat-b
-         * - قیمت: min_price/max_price استاندارد WooCommerce.
-         */
-        function buildBrowserUrl() {
-
+        function buildUrl() {
             var cats = getSelectedCategories();
             var price = getPriceValues();
             var url;
 
             if (cats.length === 1) {
-
                 var termUrl = getTermUrlBySlug(cats[0]);
-
-                if (termUrl) {
-                    url = new URL(termUrl, window.location.origin);
-                } else {
-                    url = new URL(spfData.shopUrl, window.location.origin);
-                }
-
+                url = new URL(termUrl || spfData.shopUrl, window.location.origin);
             } else {
-
                 url = new URL(spfData.shopUrl, window.location.origin);
 
                 if (cats.length > 1) {
                     url.searchParams.set('product_cat', cats.join(','));
                 }
             }
-
-            [
-                'spf_cats',
-                'spf_min_price',
-                'spf_max_price',
-                'paged'
-            ].forEach(function(param) {
-                url.searchParams.delete(param);
-            });
 
             if (price.min > absMin) {
                 url.searchParams.set('min_price', Math.round(price.min));
@@ -117,110 +96,143 @@
                 url.searchParams.delete('max_price');
             }
 
-            return url;
+            url.searchParams.delete('paged');
+            url.searchParams.delete('spf_cats');
+            url.searchParams.delete('spf_min_price');
+            url.searchParams.delete('spf_max_price');
+
+            return url.toString();
         }
 
-        function findProductsContainer() {
-
+        function getProductsSelector() {
             var selectors = [
-                '.site-content ul.products',
-                '.main-page-wrapper ul.products',
+                '.wd-products ul.products',
+                '.wd-products-element ul.products',
                 '.shop-content ul.products',
-                '.shop-container ul.products',
+                '.site-content ul.products',
                 'ul.products'
             ];
 
             for (var i = 0; i < selectors.length; i++) {
-                var $found = $(selectors[i]);
+                var $el = $(selectors[i]).first();
 
-                if ($found.length) {
-                    return $found.first();
+                if ($el.length) {
+                    return $el;
                 }
             }
 
             return $();
         }
 
-        function applyFilter($wrap) {
+        function replaceFromResponse(html) {
+            var $page = $('<div>').append($.parseHTML(html, document, true));
+            var $currentProducts = getProductsSelector();
 
-            var cats = getSelectedCategories();
-            var price = getPriceValues();
-            var $products = findProductsContainer();
+            if (!$currentProducts.length) {
+                return false;
+            }
+
+            var $newProducts = $page.find('ul.products').first();
+
+            if (!$newProducts.length) {
+                return false;
+            }
+
+            $currentProducts.replaceWith($newProducts);
+
+            var replacements = [
+                ['.woocommerce-result-count', '.woocommerce-result-count'],
+                ['.woocommerce-ordering', '.woocommerce-ordering'],
+                ['.wd-shop-tools .woocommerce-result-count', '.wd-shop-tools .woocommerce-result-count'],
+                ['.wd-shop-tools .woocommerce-ordering', '.wd-shop-tools .woocommerce-ordering']
+            ];
+
+            replacements.forEach(function(pair) {
+                var $old = $(pair[0]).first();
+                var $new = $page.find(pair[1]).first();
+
+                if ($old.length && $new.length) {
+                    $old.replaceWith($new);
+                }
+            });
+
+            var $oldPagination = $('.woocommerce-pagination').first();
+            var $newPagination = $page.find('.woocommerce-pagination').first();
+
+            if ($oldPagination.length && $newPagination.length) {
+                $oldPagination.replaceWith($newPagination);
+            } else if ($newPagination.length && !$oldPagination.length) {
+                $('.products').last().after($newPagination);
+            } else if ($oldPagination.length && !$newPagination.length) {
+                $oldPagination.remove();
+            }
+
+            var newTitle = $page.find('title').text();
+            if (newTitle) {
+                document.title = newTitle;
+            }
+
+            var $newH1 = $page.find('h1').first();
+            var $oldH1 = $('h1').first();
+
+            if ($newH1.length && $oldH1.length) {
+                $oldH1.html($newH1.html());
+            }
+
+            $(document.body).trigger('updated_wc_div');
+            $(document.body).trigger('wc_fragments_refreshed');
+            $(document).trigger('wdShopPageInit');
+            $(window).trigger('resize');
+
+            return true;
+        }
+
+        function loadUrl(url, pushState) {
+            if (request && request.readyState !== 4) {
+                request.abort();
+            }
+
+            var $products = getProductsSelector();
 
             if (!$products.length) {
-                window.location.href = buildBrowserUrl().toString();
+                window.location.href = url;
                 return;
             }
 
             $wrap.addClass('spf-loading');
             $products.addClass('spf-products-loading');
 
-            $.ajax({
-                url: spfData.ajaxUrl,
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    action: 'spf_filter',
-                    nonce: spfData.nonce,
-                    cats: cats,
-                    min: price.min > absMin ? price.min : 0,
-                    max: price.max < absMax ? price.max : 0,
-                    paged: 1
+            request = $.ajax({
+                method: 'GET',
+                url: url,
+                dataType: 'html',
+                cache: true,
+                headers: {
+                    'X-SPF': '1'
                 }
-            }).done(function(response) {
-
-                if (
-                    response &&
-                    response.success &&
-                    response.data &&
-                    typeof response.data.html !== 'undefined'
-                ) {
-
-                    var $newProducts = $(response.data.html).filter('.products');
-
-                    if (!$newProducts.length) {
-                        $newProducts = $(response.data.html).find('.products').first();
-                    }
-
-                    if ($newProducts.length) {
-
-                        $products.replaceWith($newProducts.first());
-
-                        $(document.body).trigger('updated_wc_div');
-
+            }).done(function(html) {
+                if (replaceFromResponse(html)) {
+                    if (pushState) {
                         window.history.pushState(
-                            {
-                                spf: true,
-                                cats: cats,
-                                min: price.min,
-                                max: price.max
-                            },
+                            { spf: true },
                             '',
-                            buildBrowserUrl().toString()
+                            url
                         );
-
-                    } else {
-                        window.location.href = buildBrowserUrl().toString();
                     }
-
                 } else {
-                    window.location.href = buildBrowserUrl().toString();
+                    window.location.href = url;
                 }
-
-            }).fail(function() {
-
-                window.location.href = buildBrowserUrl().toString();
-
+            }).fail(function(xhr, status) {
+                if (status !== 'abort') {
+                    window.location.href = url;
+                }
             }).always(function() {
-
                 $wrap.removeClass('spf-loading');
                 $('.spf-products-loading').removeClass('spf-products-loading');
-
             });
         }
 
         $minRange.on('input', function() {
-
             if (parseFloat($minRange.val()) > parseFloat($maxRange.val()) - step) {
                 $minRange.val(parseFloat($maxRange.val()) - step);
             }
@@ -229,7 +241,6 @@
         });
 
         $maxRange.on('input', function() {
-
             if (parseFloat($maxRange.val()) < parseFloat($minRange.val()) + step) {
                 $maxRange.val(parseFloat($minRange.val()) + step);
             }
@@ -242,20 +253,18 @@
         var priceTimer;
 
         $minRange.add($maxRange).on('input', function() {
-
             clearTimeout(priceTimer);
 
             priceTimer = setTimeout(function() {
-                applyFilter($wrap);
+                loadUrl(buildUrl(), true);
             }, 600);
         });
 
         $wrap.on('change', '.spf-cat-checkbox', function() {
-            applyFilter($wrap);
+            loadUrl(buildUrl(), true);
         });
 
         $wrap.on('click', '.spf-toggle', function(e) {
-
             e.preventDefault();
             e.stopPropagation();
 
@@ -271,9 +280,7 @@
     }
 
     function initAll() {
-
         $('.spf-wrap').each(function() {
-
             if (!$(this).data('spf-init')) {
                 $(this).data('spf-init', true);
                 initFilter($(this));
@@ -283,8 +290,127 @@
 
     $(document).ready(initAll);
 
-    $(window).on('elementor/frontend/init', function() {
+    window.addEventListener('popstate', function() {
+        var $wrap = $('.spf-wrap').first();
 
+        if ($wrap.length) {
+            loadFilterUrl(window.location.href, $wrap);
+        }
+    });
+
+    function loadFilterUrl(url, $wrap) {
+        if (request && request.readyState !== 4) {
+            request.abort();
+        }
+
+        var $products = getGlobalProductsSelector();
+
+        if (!$products.length) {
+            window.location.reload();
+            return;
+        }
+
+        $wrap.addClass('spf-loading');
+        $products.addClass('spf-products-loading');
+
+        request = $.ajax({
+            method: 'GET',
+            url: url,
+            dataType: 'html',
+            cache: true,
+            headers: {
+                'X-SPF': '1'
+            }
+        }).done(function(html) {
+            if (!replaceGlobalFromResponse(html)) {
+                window.location.reload();
+            }
+        }).fail(function(xhr, status) {
+            if (status !== 'abort') {
+                window.location.reload();
+            }
+        }).always(function() {
+            $wrap.removeClass('spf-loading');
+            $('.spf-products-loading').removeClass('spf-products-loading');
+        });
+    }
+
+    function getGlobalProductsSelector() {
+        var selectors = [
+            '.wd-products ul.products',
+            '.wd-products-element ul.products',
+            '.shop-content ul.products',
+            '.site-content ul.products',
+            'ul.products'
+        ];
+
+        for (var i = 0; i < selectors.length; i++) {
+            var $el = $(selectors[i]).first();
+
+            if ($el.length) {
+                return $el;
+            }
+        }
+
+        return $();
+    }
+
+    function replaceGlobalFromResponse(html) {
+        var $page = $('<div>').append($.parseHTML(html, document, true));
+        var $currentProducts = getGlobalProductsSelector();
+        var $newProducts = $page.find('ul.products').first();
+
+        if (!$currentProducts.length || !$newProducts.length) {
+            return false;
+        }
+
+        $currentProducts.replaceWith($newProducts);
+
+        var pairs = [
+            '.woocommerce-result-count',
+            '.woocommerce-ordering'
+        ];
+
+        pairs.forEach(function(selector) {
+            var $old = $(selector).first();
+            var $new = $page.find(selector).first();
+
+            if ($old.length && $new.length) {
+                $old.replaceWith($new);
+            }
+        });
+
+        var $oldPagination = $('.woocommerce-pagination').first();
+        var $newPagination = $page.find('.woocommerce-pagination').first();
+
+        if ($oldPagination.length && $newPagination.length) {
+            $oldPagination.replaceWith($newPagination);
+        } else if ($oldPagination.length) {
+            $oldPagination.remove();
+        } else if ($newPagination.length) {
+            $('.products').last().after($newPagination);
+        }
+
+        var title = $page.find('title').text();
+        if (title) {
+            document.title = title;
+        }
+
+        var $newH1 = $page.find('h1').first();
+        var $oldH1 = $('h1').first();
+
+        if ($newH1.length && $oldH1.length) {
+            $oldH1.html($newH1.html());
+        }
+
+        $(document.body).trigger('updated_wc_div');
+        $(document).trigger('wdShopPageInit');
+        $(window).trigger('resize');
+
+        return true;
+    }
+
+    $(window).on('elementor/frontend/init', function() {
         if (
             typeof elementorFrontend !== 'undefined' &&
             elementorFrontend.hooks
@@ -292,7 +418,6 @@
             elementorFrontend.hooks.addAction(
                 'frontend/element_ready/spf_smart_filter.default',
                 function($scope) {
-
                     var $wrap = $scope.find('.spf-wrap');
 
                     if ($wrap.length && !$wrap.data('spf-init')) {
